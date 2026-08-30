38fe1f20b05f3826707e9e99bbe69a438e576146
afghantanha737-alt <afghantanha737@gmail.com>
2026-08-30 04:52:28 +0430

    Update Task.js

--- models/Task.js
+++ models/Task.js
@@ -3,9 +3,10 @@ const mongoose = require('mongoose');
 const taskSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, default: '' },
-  link: { type: String, default: '' }, // مثلا لینک کانال/گروه/یوتیوب برای انجام تسک
+  link: { type: String, default: '' },
   pointsReward: { type: Number, required: true },
   active: { type: Boolean, default: true },
+  verifyChannel: { type: String, default: '' },
   createdAt: { type: Date, default: Date.now }
 });
 

2f0a2949d6c869b00065d4fbd41d71c91cb3abce
afghantanha737-alt <afghantanha737@gmail.com>
2026-09-20 14:31:08 +0430

    Add files via upload

--- models/User.js
+++ models/User.js
@@ -15,6 +15,8 @@ const userSchema = new mongoose.Schema(
     totalCheckins: { type: Number, default: 0, min: 0 },
     lastCheckIn: { type: Date, default: null },
     spinChances: { type: Number, default: 0, min: 0 },
+    adWatchesToday: { type: Number, default: 0, min: 0 },
+    lastAdWatchDate: { type: Date, default: null },
 
     referralCode: { type: String, required: true, unique: true, index: true },
     referredBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null },
